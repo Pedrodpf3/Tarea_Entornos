698ebf0818188e994d64845f5d9b1a9b485a1389
Piedad <Piedad@Ord-despacho>
2023-03-21 19:56:42 +0100

    Refactorización realizada

--- src/Hogar/Hogar.java
+++ src/Hogar/Hogar.java
@@ -1,35 +1,25 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Hogar;
 
-/**
- *
- * @author paqui
- */
-public class Hogar {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
+public class Hogar 
+{
+    public static void main(String[] args) 
+    {
+        
+      
+        
+    }
+    
+    public void imprimirVivienda(Vivienda v) 
+    {
         Vivienda casa1 = new Vivienda(120000);
         
-        double precio;
-        int numHabitaciones;
-        double superficie;
-        boolean parking;
-        String estado;
-        String propietario;
-        
-        System.out.println("Precio: " + casa1.precio);
-        System.out.println("Número de habitaciones: "+ casa1.numHabitaciones);
-        System.out.println("Superficie en m2: " + casa1.superficie);
-        System.out.println("¿Dispone de parking?: " + casa1.parking);
-        System.out.println("Estado: "+ casa1.estado);
-        System.out.println("Propietario: "+ casa1.propietario);
+        System.out.println("Precio: " + casa1.getPrecio());
+        System.out.println("Número de habitaciones: "+ casa1.getnumHabitaciones());
+        System.out.println("Superficie en m2: " + casa1.getsuperficie());
+        System.out.println("¿Dispone de parking?: " + casa1.getparking());
+        System.out.println("Estado: "+ casa1.getestado());
+        System.out.println("Propietario: "+ casa1.getpropietario());
     }
+
     
 }
--- src/Hogar/Vivienda.java
+++ src/Hogar/Vivienda.java
@@ -1,31 +1,82 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Hogar;
 
-/**
- *
- * @author paqui
- */
-public class Vivienda {
-    
-    double precio;
-    int numHabitaciones;
-    double superficie;
-    boolean parking;
-    String estado;
-    String propietario;
+public class Vivienda 
+{  
+    private double precio; 
+    private int numHabitaciones; 
+    private double superficie;
+    private boolean parking; 
+    private String estado; 
+    private String propietario; 
     
     public Vivienda (double precio)
     {
         this.precio = precio;
     }
-    
-    public void actualizarPrecio()
+    public Vivienda (double pre,int n, double s, boolean par, String e, String p)
     {
-        precio = precio - precio * 0.05;
+        this.precio = pre;
+        this.numHabitaciones = n;
+        this.superficie = s;
+        this.parking = par;
+        this.estado = e;
+        this.propietario = p;
+    }
+    public void actualizarPrecio(int descuento)
+    {
+        this.precio = this.precio - descuento;
+    }
+    double getPrecio()
+    {
+         return this.precio;   
+    }
+    int getnumHabitaciones()
+    {
+         return this.numHabitaciones;   
+    }
+    double getsuperficie()
+    {
+         return this.superficie;   
+    }
+    boolean getparking()
+    {
+         return this.parking;   
+    }    
+    String getestado()
+    {
+         return this.estado;   
+    }   
+    String getpropietario()
+    {
+         return this.propietario;   
+    }
+    void setPrecio(double x)
+    {
+        this.precio = x;   
     }
     
+    void setnumHabitaciones(int i)
+    {
+        this.numHabitaciones = i;   
+    }
+    
+    void setsuperficie(double d)
+    {
+        this.superficie = d;   
+    }
+    
+    void setparking(boolean k)
+    {
+        this.parking = k;   
+    }
+    
+    void setestado(String h)
+    {
+        this.estado = h;   
+    }
+    
+    void setpropietario(String j)
+    {
+        this.propietario = j;   
+    }    
 }
